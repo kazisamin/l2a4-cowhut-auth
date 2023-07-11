488a2974f6ab7ce013ede1656746a9c296817068
kazisamin <azharul.samin@gmail.com>
2023-07-12 02:49:27 +0600

    prettier lint setup

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,25 @@
-import express, { Application, NextFunction, Request, Response } from "express";
-const app: Application = express();
-import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
 
-const port = 3000;
+const port = 3000
 
-app.use(cors());
+app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 
 //testing
-app.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  console.log("working");
-  Promise.reject(new Error("Unhandled Promise Rejection"));
-});
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  console.log('working')
+  Promise.reject(new Error('Unhandled Promise Rejection'))
+})
 
-app.listen(port);
+app.listen(port)
 
-export default app;
+export default app
