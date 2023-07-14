b54cbe186fcf2f437ab459131b3e5488ac800773
kazisamin <azharul.samin@gmail.com>
2023-07-14 21:04:38 +0600

    user model interface created

---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,11 @@
+import { Model, Types } from 'mongoose'
+
+export type IUser = {
+  id: string
+  role: string
+  password: string
+  seller?: Types.ObjectId | ISeller
+  buyer?: Types.ObjectId | IBuyer
+}
+
+export type UserModel = Model<IUser, Record<string, unknown>>
