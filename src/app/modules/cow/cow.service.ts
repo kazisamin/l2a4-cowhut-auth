f797fb500dda19a6a1e4dfc396a31b91987b13be
kazisamin <azharul.samin@gmail.com>
2023-08-18 16:36:27 +0600

    cow service added

---
+++ src/app/modules/cow/cow.service.ts
@@ -0,0 +1,117 @@
+import { SortOrder } from 'mongoose';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { cowSearchableFields } from './cow.constants';
+import { ICow, ICowFilters } from './cow.interfaces';
+import { Cow } from './cow.model';
+
+const createCow = async (
+  payload: ICow
+): Promise<ICow | null> => {
+  const result = (await Cow.create(payload)).populate(
+    'seller'
+  );
+  return result;
+};
+
+const getSingleCow = async (
+  id: string
+): Promise<ICow | null> => {
+  const result = await Cow.findById(id).populate(
+    'seller'
+  );
+
+  return result;
+};
+
+const getAllCows = async (
+  filters: ICowFilters,
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<ICow[]>> => {
+  const { limit, page, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  // Extract searchTerm to implement search query
+  const { searchTerm, ...filtersData } = filters;
+
+  const andConditions = [];
+
+  // Search needs $or for searching in specified fields
+  if (searchTerm) {
+    andConditions.push({
+      $or: cowSearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $paginationOptions: 'i',
+        },
+      })),
+    });
+  }
+
+  // Filters needs $and to fullfill all the conditions
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+
+  // Dynamic  Sort needs  field to  do sorting
+  const sortConditions: { [key: string]: SortOrder } = {};
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+
+  // If there is no condition , put {} to give all data
+  const whereConditions =
+    andConditions.length > 0 ? { $and: andConditions } : {};
+
+  const result = await Cow.find(whereConditions)
+    .populate('seller')
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await Cow.countDocuments(whereConditions);
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const updateCow = async (
+  id: string,
+  payload: Partial<ICow>
+): Promise<ICow | null> => {
+  const result = await Cow.findOneAndUpdate(
+    { _id: id },
+    payload,
+    {
+      new: true,
+    }
+  ).populate('seller');
+
+  return result;
+};
+
+const deleteCow = async (
+  id: string
+): Promise<ICow | null> => {
+  const result = await Cow.findByIdAndDelete(id);
+  return result;
+};
+
+export const CowService = {
+  createCow,
+  getSingleCow,
+  getAllCows,
+  updateCow,
+  deleteCow,
+};
